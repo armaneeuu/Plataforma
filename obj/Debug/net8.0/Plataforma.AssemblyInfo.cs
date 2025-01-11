0133fc6d27a672f6c661a06b8e8c87a51e92a73e
armaneeuu <armaninteresado@hotmail.com>
2025-01-10 20:24:10 -0500

    Primer commit con archivos del proyecto

--- obj/Debug/net8.0/Plataforma.AssemblyInfo.cs
+++ obj/Debug/net8.0/Plataforma.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plataforma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb2649005cd7ff149a89c0aeaf762c2c68ff8a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plataforma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plataforma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
